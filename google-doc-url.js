e8b7ac3ddefd36895e5b04a0bcdd8cfafd95d584
etidona <etidona@me.com>
2020-04-17 11:48:28 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1tvVD3nvkkP8DFetFZm_l_vuHXxId9pziFnaiDvACBhw/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bm4EggJvhF6Md48aK6NGK8pYQMyAPC_ceu8teOFdQ8E/edit#gid=0';
